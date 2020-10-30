20896dac23460d7c25d2119aed4b5408563e7805
doha <58391499+dawnstudiescs@users.noreply.github.com>
2020-10-30 12:36:31 +0100

    making the bot run

---
+++ lib/bot/__init__.py
@@ -0,0 +1,44 @@
+from sys import version
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord.ext.commands import Bot as BotBase
+
+PREFIX = "+"
+OWNER_IDS = [424472476106489856]
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+
+    def run(self, version):
+        self.VERSION = version
+
+        with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
+
+        print("running Chopper...")
+        super().run(self.TOKEN, reconnect=True)
+
+    async def on_connect(self):
+        print("Chopper connected.")
+
+    async def on_disconnect(self):
+        print("Chopper disconnected.")
+
+    async def on_ready(self):
+        if not self.ready:
+            self.ready = True
+            self.guild = self.get_guild(704165535226527765)
+            print("Chopper ready.")
+
+        else:
+            print("Chopper reconnected.")
+
+    async def on_message(self, message):
+        pass
+
+bot = Bot()
